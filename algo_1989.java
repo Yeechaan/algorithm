ed170c43deb2a818afc94c270ed2a6da4597e24e
YechanLee <yechanharry@gmail.com>
2019-06-24 15:55:37 +0900

    1989. 초심자의 회문 검사 D2

---
+++ algo_1989.java
@@ -0,0 +1,32 @@
+package algo0624;
+
+import java.util.Scanner;
+
+public class algo_1 {
+
+	public static void main(String []args) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+ 
+        for(int test_case = 1; test_case <= T; test_case++)
+        {           
+            String text = sc.next();
+            char[] arr = text.toCharArray();
+            int result = 1;
+            for(int i=0; i<(text.length()/2);i++) {
+                if(arr[i]!=arr[arr.length-i-1]) {
+                	result = 0;
+                }
+                 
+            }
+            if(result==0) {
+                System.out.println("#"+test_case+" "+result);
+            } else {
+                System.out.println("#"+test_case+" "+result);
+            }
+        }
+		
+	}
+}
