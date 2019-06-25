1029ee0e9347e6242d7a3490c5847023a0b0d77f
YechanLee <yechanharry@gmail.com>
2019-06-25 10:14:47 +0900

    1986. 지그재그 숫자 D2

---
+++ algo_1986.java
@@ -0,0 +1,29 @@
+package algo0624;
+
+import java.util.Scanner;
+
+public class algo_1986 {
+	
+	public static void main(String []args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		int test_case;
+		int number;
+		
+		test_case = scanner.nextInt();
+		for(int i=1 ; i<=test_case ; i++) {
+			int sum = 0;
+			number = scanner.nextInt();
+			
+			for(int j=1 ; j<=number ; j++) {
+				if(j%2 == 1) {
+					sum+=j;
+				} else {
+					sum-=j;
+				}
+			}
+			System.out.println("#" + i + " " + sum);
+		}
+		
+	}
+
+}
