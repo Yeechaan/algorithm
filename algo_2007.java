07ab883cc1848778cec4d23d68a120d8df98c339
YechanLee <yechanharry@gmail.com>
2019-06-27 23:14:25 +0900

    2007. 패턴 마디의 길이 D2

---
+++ algo_2007.java
@@ -0,0 +1,36 @@
+package algo0624;
+
+import java.util.Scanner;
+
+public class algo2007 {
+
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+  
+        for(int test_case = 1; test_case <= T; test_case++)
+        {           
+            String text = sc.next();
+            
+            String textPattern = text;
+            String textTemp = "";
+            int i = 1;
+            int j = 1;
+            
+            while(textPattern.equals(textTemp) != true && textTemp.length() < text.length()/2-1){
+            	textPattern = text.substring(0, i+1);
+            	j = i+1;
+            	textTemp = text.substring(j, j+i+1);
+                System.out.println("#"+textPattern);
+
+                System.out.println("#"+textTemp);
+
+                i++;
+            }
+            
+            System.out.println("#"+test_case+" "+i);
+        }
+	}
+	
+}
