2ad4389c47d314c08fb1ac4ece36b5999d3700a8
Yeechaan <41672138+Yeechaan@users.noreply.github.com>
2019-07-10 22:37:26 +0900

    1288. 새로운 불면증 치료법 D2

---
+++ algo_1288.java
@@ -0,0 +1,30 @@
+package algo0624;
+
+import java.util.Scanner;
+
+public class algo_1288 {
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		for(int i=1 ; i<=t ; i++) {
+			int n = sc.nextInt();
+
+			int[] nums = new int[10];
+			int sum=0;
+			
+			while(sum != 45) {
+				String sn = Integer.toString(n);
+				int s = sn.length();
+				
+				for(int j=0 ; j<s ; j++) {
+					
+				}
+			}
+			
+			//System.out.println("string : "+sn.charAt(0));
+		}
+		
+	}
+
+}
