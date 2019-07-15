200ea63396394481bc5e532b04cd4d69e81676ac
YechanLee <yechanharry@gmail.com>
2019-07-15 11:57:26 +0900

    1288. 새로운 불면증 치료법 D2

--- algo_1288.java
+++ algo_1288.java
@@ -12,19 +12,57 @@ public class algo_1288 {
 
 			int[] nums = new int[10];
 			int sum=0;
+			int mul=0;
 			
-			while(sum != 45) {
-				String sn = Integer.toString(n);
+			while(sum != 10) {
+				sum=0;
+				mul++;
+				int number = n*mul;
+				String sn = Integer.toString(number);
 				int s = sn.length();
 				
 				for(int j=0 ; j<s ; j++) {
 					
+					switch(sn.charAt(j)) {
+					case '0':
+						nums[0]=1;
+						break;
+					case '1':
+						nums[1]=1;
+						break;
+					case '2':
+						nums[2]=1;
+						break;
+					case '3':
+						nums[3]=1;
+						break;
+					case '4':
+						nums[4]=1;
+						break;
+					case '5':
+						nums[5]=1;
+						break;
+					case '6':
+						nums[6]=1;
+						break;
+					case '7':
+						nums[7]=1;
+						break;
+					case '8':
+						nums[8]=1;
+						break;
+					case '9':
+						nums[9]=1;
+						break;
+					}
+				}
+				for(int k=0 ; k<10 ; k++) {
+					sum += nums[k];
 				}
 			}
 			
-			//System.out.println("string : "+sn.charAt(0));
+			System.out.println("#" + i + " " + mul*n);
 		}
 		
 	}
-
 }
