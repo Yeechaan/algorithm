8bc432e28e411ad24c49c89d35c012005d86cc41
YechanLee <yechanharry@gmail.com>
2019-08-12 22:14:47 +0900

    6730. 장애물 경주 난이도 D3

---
+++ Level_D3/algo_6730.java
@@ -0,0 +1,36 @@
+package algo_D3;
+
+import java.util.Scanner;
+
+public class algo_6730 {
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+		int test = sc.nextInt();
+		
+		for(int i=1 ; i<=test ; i++) {
+			int number = sc.nextInt();
+			int[] blocks = new int[number];
+			int high=0, low=0, temp;
+			
+			for(int j=0 ; j<number ; j++) {
+				blocks[j] = sc.nextInt();
+			}
+			
+			for(int j=0 ; j<number-1 ; j++) {
+				if(blocks[j] < blocks[j+1]) {
+					temp = blocks[j+1] - blocks[j];
+					if(temp > high) {
+						high = temp;
+					}
+				} else if(blocks[j] > blocks[j+1]) {
+					temp = blocks[j] - blocks[j+1];
+					if(temp > low) {
+						low = temp;
+					}
+				}
+			}
+			System.out.println("#" + i + " " + high + " " + low);
+		}
+		
+	}
+}
