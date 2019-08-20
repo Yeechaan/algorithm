64a859d55150a6b7702d10a2ebeba57c36461383
YechanLee <yechanharry@gmail.com>
2019-08-20 12:21:05 +0900

    8016. 홀수 피라미드 D3

---
+++ Level_D3/algo_8016.java
@@ -0,0 +1,22 @@
+package algo_D3;
+
+import java.util.Scanner;
+
+public class algo_8016 {
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+		int test = sc.nextInt();
+		
+		for(int i=1 ; i<=test ; i++) {
+			long floor = sc.nextInt();
+			long leftNum=1, rightNum=1;
+			
+			if(floor > 1) {
+				leftNum = (floor-1)*(floor-1)*2 + 1;
+				rightNum = (floor)*(floor)*2 - 1;
+			}
+			System.out.println("#" + i + " " + leftNum + " " + rightNum);
+		}
+		sc.close();
+	}
+}
