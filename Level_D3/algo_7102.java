d0561bebd7c0a885970b8a96d9441f32129acf8f
YechanLee <yechanharry@gmail.com>
2019-08-27 09:59:09 +0900

    7102. 준홍이의 카드놀이 D3

---
+++ Level_D3/algo_7102.java
@@ -0,0 +1,40 @@
+package algo_D3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class algo_7102 {
+	public static void main(String []args) throws Exception{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int test = Integer.parseInt(bf.readLine());
+		for(int i=1 ; i<=test ; i++) {
+			st = new StringTokenizer(bf.readLine());
+			int firCard = Integer.parseInt(st.nextToken());
+			int secCard = Integer.parseInt(st.nextToken());
+			int sum=1, firSum, secSum;
+			
+			System.out.printf("#%d ", i);
+			if(firCard == secCard) {
+				sum = sum + secCard;
+				System.out.printf("%d", sum);
+			}
+			else {
+				if(firCard > secCard) {
+					firSum = sum + secCard;
+					secSum = sum + firCard;
+				}else {
+					secSum = sum + secCard;
+					firSum = sum + firCard;
+				}
+				for(int j=firSum ; j<=secSum ; j++) {
+					System.out.printf("%d ", j);
+				}
+			}
+			int max = Integer.MIN_VALUE;
+			System.out.printf("\n + %d", max);
+		}
+	}
+}
