f538543a8eaffc7c0b2eed63ee4a6d962a9681c8
YechanLee <yechanharry@gmail.com>
2019-09-17 13:08:27 +0900

    4406. 모음이 보이지 않는 사람 D3

---
+++ Level_D3/algo_4406.java
@@ -0,0 +1,23 @@
+package algo_D3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class algo_4406 {
+	public static void main(String []args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		
+		for(int i=1 ; i<=test ; i++) {
+			String word = br.readLine();
+			String resWord = "";
+			
+			for(int j=0 ; j<word.length() ; j++) {
+				if(word.charAt(j)!='a' && word.charAt(j)!='i' && word.charAt(j)!='o' && word.charAt(j)!='u' && word.charAt(j)!='e') {
+					resWord += word.charAt(j);
+				}
+			}
+			System.out.println("#" + i + " " + resWord);
+		}	
+	}
+}
