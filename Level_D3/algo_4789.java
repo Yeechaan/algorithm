2eda968563a2a751d864b0e253e3b8d4e5ec6bad
YechanLee <yechanharry@gmail.com>
2019-10-01 18:37:23 +0900

    4789. 성공적인 공연 기획 D3

---
+++ Level_D3/algo_4789.java
@@ -0,0 +1,28 @@
+package algo_D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class algo_4789 {
+	public static void main(String []args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		
+		for(int i=1 ; i<=test ; i++) {
+			String audience = br.readLine();
+			int sumAudi=0 , minAudi=0;
+			
+			for(int j=0 ; j<audience.length() ; j++) {
+				if(audience.charAt(j) == '0' && sumAudi<j+1) {
+					minAudi++;
+					sumAudi++;
+				} else {
+					sumAudi += Character.getNumericValue(audience.charAt(j));
+				}
+			}
+			System.out.println("#" + i + " " + minAudi);
+		}
+	}
+
+}
