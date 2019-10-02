f26fe6d217d3a82ca6b2692d0560200a618fede7
YechanLee <yechanharry@gmail.com>
2019-10-02 19:54:50 +0900

    4371. 항구에 들어오는 배 D3

---
+++ Level_D3/algo_4371.java
@@ -0,0 +1,37 @@
+package algo_D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class algo_4371 {
+	public static void main(String []args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		
+		for(int i=1 ; i<=test ; i++) {
+			int number = Integer.parseInt(br.readLine());
+			int[] boats = new int[number];
+			int numBoat = 0;
+			
+			int boat = Integer.parseInt(br.readLine());
+			boats[numBoat] = Integer.parseInt(br.readLine()) - boat;
+			numBoat++;
+			
+			for(int j=0 ; j<number-2 ; j++) {
+				boat = Integer.parseInt(br.readLine());
+				int k=0;
+				while(k<numBoat && (boat-1)%boats[k]!=0) {
+					
+					k++;
+				}
+				if(k == numBoat) {
+					boats[numBoat] = boat - 1;
+					numBoat++;
+				}
+				
+			}
+			System.out.println("#" + i + " " + numBoat);
+		}
+	}
+}
