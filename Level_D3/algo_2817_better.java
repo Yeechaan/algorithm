9f217cca4ffdcbff474224d7af056856bed13bfc
YechanLee <yechanharry@gmail.com>
2019-10-04 18:19:52 +0900

    2817. 부분 수열의 합 D3

---
+++ Level_D3/algo_2817_better.java
@@ -0,0 +1,42 @@
+package algo_D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class algo_2817_better {
+	static int n,k;
+    static int[] arr;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=1;i<=tc;i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
+            arr = new int[n];
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<n;j++)
+                arr[j] = Integer.parseInt(st.nextToken());
+            result = 0;
+            solve(0,0);
+            sb.append("#"+i+" "+result+"\n");
+        }
+        System.out.println(sb);
+    }
+    
+    static int result;
+    private static void solve(int idx,int sum) {
+        if(sum==k) {
+            result++;
+            return;
+        }
+        if(sum>k || idx>=n) return;
+        // 자기 자신을 포함
+        solve(idx+1,sum+arr[idx]);
+        // 자기 자신을 미포함
+        solve(idx+1,sum);
+    }
+}
