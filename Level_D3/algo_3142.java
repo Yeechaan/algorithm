41f9d5b45cb2742ea88643b3be7fd074e7706674
YechanLee <yechanharry@gmail.com>
2019-10-07 16:26:48 +0900

    3142. 영준이와 신비한 뿔의 숲 D3

---
+++ Level_D3/algo_3142.java
@@ -0,0 +1,25 @@
+package algo_D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class algo_3142 {
+	public static void main(String []args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		
+		for(int i=1 ; i<=test ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int numHorn = Integer.parseInt(st.nextToken());
+			int numAni = Integer.parseInt(st.nextToken());
+			int numUni=0, numTwin=0;
+			
+			numTwin = numHorn - numAni;
+			numUni = numAni - numTwin;
+			
+			System.out.println("#" + i + " " + numUni + " " + numTwin);
+		}
+	}
+}
