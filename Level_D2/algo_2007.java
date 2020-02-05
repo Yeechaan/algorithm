69bcf3323a3a9f77a89d67391249c8f13f5e7aab
YechanLee <yechanharry@gmail.com>
2020-02-05 23:19:31 +0900

    2007. 패턴 마디의 길이 D2

--- Level_D2/algo_2007.java
+++ Level_D2/algo_2007.java
@@ -5,29 +5,38 @@ import java.util.Scanner;
 public class algo2007 {
 
 	public static void main(String []args) {
-		Scanner sc = new Scanner(System.in);
-        int T;
-        T=sc.nextInt();
+	    Scanner sc = new Scanner(System.in);
+	    int T;
+                T=sc.nextInt();
   
-        for(int test_case = 1; test_case <= T; test_case++)
-        {           
-            String text = sc.next();
+        	    for(int test_case = 1; test_case <= T; test_case++) {           
+                    String text = sc.next();
             
-            String textPattern = text;
-            String textTemp = "";
-            int i = 1;
-            int j = 1;
+                    String textPattern = text;
+                    String textTemp = "";
+                    int i = 1;
+                    int j = 1;
             
-            while(textPattern.equals(textTemp) != true && textTemp.length() < text.length()/2-1){
+                    while(textPattern.equals(textTemp) != true && textTemp.length() < text.length()/2-1){
             	textPattern = text.substring(0, i+1);
             	j = i+1;
             	textTemp = text.substring(j, j+i+1);
 
-                i++;
-            }
+                        i++;
+                    }
             
-            System.out.println("#"+test_case+" "+i);
-        }
-	}
-	
+                    System.out.println("#"+test_case+" "+i);
+                }
+            }
+/*
+Input
+3       
+KOREAKOREAKOREAKOREAKOREAKOREA
+SAMSUNGSAMSUNGSAMSUNGSAMSUNGSA
+GALAXYGALAXYGALAXYGALAXYGALAXY     
+Output
+#1 5
+#2 7
+#3 6
+*/
 }
