4af1e48eccd8c1179d025a469958e34c28f936f8
YechanLee <yechanharry@gmail.com>
2020-02-21 20:55:36 +0900

    9480. 민정이와 광직이의 알파벳 공부

--- Level_D3/algo_9480.java
+++ Level_D3/algo_9480.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 public class algo_9480 {
 	static public int count;
+	static public ArrayList<String> words;
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,18 +15,17 @@ public class algo_9480 {
 		
 		for(int i=1 ; i<=test ; i++) {
 			int size = Integer.parseInt(br.readLine());
-			ArrayList<String> words = new ArrayList<String>();
+			words = new ArrayList<String>();
 			int [] arr = new int[size];
 			
-//			for(int j=0 ; j<size ; j++) {
-//				words.add(br.readLine());
-//			}
+			for(int j=0 ; j<size ; j++) {
+				words.add(br.readLine());
+			}
 			
 			count = 0;
 
 			for(int j=0 ; j<size ; j++) {
-				combination(arr, 0, size, j+1, 0);				
-				
+				combination(arr, 0, size, j+1, 0);
 			}
 			
 			System.out.println("#" + i + " " + count);
@@ -35,7 +35,7 @@ public class algo_9480 {
 	
 	public static void combination(int[] arr, int index, int n, int r, int target) { 
 		if (r == 0) 
-			print(arr, index); 
+			print(arr, index);
 		else if (target == n) 
 			return; 
 		else { 
@@ -46,12 +46,43 @@ public class algo_9480 {
 	}
 		
 	public static void print(int[] arr, int length) { 
+		String temp = "";
+		
 		for (int i = 0; i < length; i++) {
-			System.out.print(arr[i]);
+			//System.out.print(arr[i]);
+			temp = temp + words.get(arr[i]);
 		}
-		System.out.println(""); 
-		count++;
-
+		
+		if(temp.length() >= 26) {
+			//checkAlphabet(temp);
+			int i=0;
+			char alpha = 'a';
+			
+			while(temp.indexOf(alpha) != -1 && i<26) {
+				alpha++;
+				i++;
+			}
+			
+			if(i==26) {
+				count++;
+			}
+		}
+	}
+	
+	public static int checkAlphabet(String word) {
+		int res=0, i=0;
+		char alpha = 'a';
+		
+		while(word.indexOf(alpha) != -1 && i<26) {
+			alpha++;
+			i++;
+		}
+		
+		if(i==26) {
+			count++;
+		}
+		
+		return res;
 	}
 
 }
