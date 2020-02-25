1468439c3cb08d7dacd921a4f62fc0c2edfcb32d
YechanLee <yechanharry@gmail.com>
2020-02-25 17:16:12 +0900

    9317. 석찬이의 받아쓰기 D3

---
+++ Level_D3/algo_9317.java
@@ -0,0 +1,30 @@
+package algo_D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class algo_9317 {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		
+		for(int i=1 ; i<=test ; i++) {
+			int size = Integer.parseInt(br.readLine());
+			String sentence = br.readLine();
+			String sentence_check = br.readLine();
+			int res =0;
+			
+			for(int j=0 ; j<size ; j++) {
+				if(sentence.charAt(j) == sentence_check.charAt(j)) {
+					res++;
+				}
+			}
+			
+			System.out.println("#" + i + " " + res);
+		}
+		
+	}
+
+}
