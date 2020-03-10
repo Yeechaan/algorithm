5cb59648da4c79fad883d0bc84d24bcb0604cdf9
YechanLee <yechanharry@gmail.com>
2020-03-10 17:36:25 +0900

    1289. 원재의 메모리 복구하기 D3

---
+++ Level_D3/algo_1289.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class algo_1289{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		
+		for(int i=1 ; i<=test ; i++) {
+			String num = br.readLine();
+			int res = 0;
+			
+			if(num.charAt(0) == '1'){
+				res++;
+			}
+			
+			for(int j=1 ; j<num.length() ; j++) {
+				if(num.charAt(j) == '0') {
+					if(num.charAt(j-1) == '1') {
+						res++;
+					}
+				}
+				else {
+					if(num.charAt(j-1) == '0') {
+						res++;
+					}
+				}
+			}
+			System.out.println("#" + i + " " + res);
+		}
+	}
+}
