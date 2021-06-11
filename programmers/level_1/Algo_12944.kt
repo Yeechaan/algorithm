b295a74c035c935a112cf10037b27e9115c2748d
YechanLee <yechanharry@gmail.com>
2021-06-11 17:10:55 +0900

    평균 구하기

---
+++ programmers/level_1/Algo_12944.kt
@@ -0,0 +1,9 @@
+package algorithm.programmers.level_1
+
+fun main() {
+    fun solution(arr: IntArray): Double = arr.average()
+
+    val result = solution(intArrayOf(1, 2, 3, 4))
+
+    println(result)
+}
