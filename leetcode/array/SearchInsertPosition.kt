a506cbbebca7aed22636fe0ba17fae0f345ee2a2
YechanLee <yechanharry@gmail.com>
2021-06-21 23:54:16 +0900

    35. Search Insert Position(최적화)

--- leetcode/array/SearchInsertPosition.kt
+++ leetcode/array/SearchInsertPosition.kt
@@ -2,22 +2,20 @@ package algorithm.leetcode.array
 
 fun main() {
     fun searchInsert(nums: IntArray, target: Int): Int {
-        var answer = -1
-
-        for (index in nums.indices) {
-            if (nums[index] >= target) {
-                answer = index
-                break
-            }
-            if (index == nums.lastIndex) {
-                answer = index + 1
+        var low = 0
+        var high = nums.size - 1
+        while (low <= high) {
+            val mid = (low + high) / 2
+            when {
+                nums[mid] == target -> return mid
+                (nums[mid] > target) -> high = mid - 1
+                else -> low = mid + 1
             }
         }
-
-        return answer
+        return low
     }
 
-    val result = searchInsert(intArrayOf(1,3,5,6), 0)
+    val result = searchInsert(intArrayOf(1, 3, 5, 6), 0)
 //    val result = searchInsert(intArrayOf(1), 0)
     println(result)
 }
