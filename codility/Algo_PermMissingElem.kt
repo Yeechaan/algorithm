18a6078abe8bdf8d23edce370e2e7e6509e29bb0
YechanLee <yechanharry@gmail.com>
2021-07-19 22:58:02 +0900

    PermMissingElem

---
+++ codility/Algo_PermMissingElem.kt
@@ -0,0 +1,16 @@
+package algorithm.codility
+
+fun main() {
+    fun solution(A: IntArray): Int {
+
+        val numberSet = (1..A.size + 1).toMutableSet()
+        A.map {
+            numberSet.remove(it)
+        }
+
+        return numberSet.first()
+    }
+
+    val result = solution(intArrayOf(2, 3, 1, 5))
+    println(result)
+}†
