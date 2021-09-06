e9f7cb9fefdc6d5d8f483ef3bbd56022c3c2f76a
Yechan Lee <yechanharry@gmail.com>
2021-09-06 23:59:19 +0900

    17. Letter Combinations of a Phone Number

---
+++ leetcode/medium/17. Letter Combinations of a Phone Number.kt
@@ -0,0 +1,38 @@
+package algorithm.leetcode.medium
+
+fun main() {
+
+    val keyMap = mapOf(
+        '2' to "abc",
+        '3' to "def",
+        '4' to "ghi",
+        '5' to "jkl",
+        '6' to "mno",
+        '7' to "pqr",
+        '8' to "suv",
+        '9' to "wxyz"
+    )
+
+    fun letterCombinations(digits: String): List<String> {
+        var answer = mutableListOf<String>()
+        if (digits.isEmpty()) return answer
+        var word: String = ""
+
+        fun dfs(cur: Int) {
+            if (cur >= digits.length) {
+                answer.add(word)
+                return ;
+            }
+            for (i in keyMap[digits[cur]]!!) {
+                word += i
+                dfs(cur+1)
+                word = word.substring(0, word.length-1)
+            }
+        }
+        dfs(0)
+        return answer
+    }
+
+    val result = letterCombinations("23")
+    println(result)
+}
