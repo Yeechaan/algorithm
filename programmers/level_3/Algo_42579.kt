ba7bf8e61f48e5fe18d58d789fb3e1e30ec05524
Yechan Lee <yechanharry@gmail.com>
2021-10-11 15:03:01 +0900

    베스트앨범 hash

--- programmers/level_3/Algo_42579.kt
+++ programmers/level_3/Algo_42579.kt
@@ -1,4 +1,19 @@
 package algorithm.programmers.level_3
 
-class Algo_42579 {
+fun main() {
+    fun solution(genres: Array<String>, plays: IntArray): IntArray {
+        val answer = genres.indices.groupBy { genres[it] }
+            .toList()
+            .sortedByDescending { it.second.sumBy { index -> plays[index] } }
+            .map { it.second.sortedByDescending { index -> plays[index] }.take(2) }
+            .flatten()
+
+        return answer.toIntArray()
+    }
+
+    val result = solution(
+        arrayOf("classic", "pop", "classic", "classic", "pop"),
+        intArrayOf(500, 600, 150, 800, 2500)
+    )
+    println(result.toList())
 }
