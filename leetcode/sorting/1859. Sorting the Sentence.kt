d4ec422558cdf3a8acb1d185136859f3090f8490
Yechan Lee <yechanharry@gmail.com>
2021-11-04 23:57:40 +0900

    1859. Sorting the Sentence

--- leetcode/sorting/1859. Sorting the Sentence.kt
+++ leetcode/sorting/1859. Sorting the Sentence.kt
@@ -2,7 +2,10 @@ package algorithm.leetcode.sorting
 
 fun main() {
     fun sortSentence(s: String): String {
-        return s.split(" ").sortedBy { it.last() }.joinToString(" ") { it.dropLast(1) }
+        return s.split(" ")
+            .sortedBy { it.last() }
+            .joinToString(" ")
+            { it.dropLast(1) }
     }
 
     val result = sortSentence("is2 sentence4 This1 a3")
